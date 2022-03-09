86a74039d2f068bf90b3a400ff45c9fbf6e5c506
Thomas Kilgour <thomas@kilgour.co>
2022-03-09 17:03:19 -0500

    initialize firebase and setup vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,15 @@
 <script>
-import { mapActions } from "pinia";
-import { useTodosStore } from "./stores/todos";
-import TodoList from "./components/TodoList.vue";
-
-export default {
-  components: {
-    TodoList,
-  },
-
-  methods: {
-    ...mapActions(useTodosStore, ["archiveTodos"]),
-  },
-
-  mounted() {
-    //  TODO: update on date change if app is left running (set a timer? interval?)
-    this.archiveTodos();
-  },
-};
+export default {};
 </script>
 
 <template>
-  <main>
-    <h1 class="text-4xl text-center mt-4">ToDo Today</h1>
-    <TodoList />
-  </main>
+  <nav>
+    <router-link :to="{ name: 'Home' }">Home</router-link>
+    <router-link :to="{ name: 'Feed' }">Feed</router-link>
+    <router-link :to="{ name: 'Register' }">Register</router-link>
+    <router-link :to="{ name: 'Login' }">Login</router-link>
+  </nav>
+  <router-view></router-view>
 </template>
 
 <style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <h1 class="text-4xl text-center mt-4">ToDo Today</h1>
+    <TodoList />
+  </main>
+</template>
+
+<script>
+import { mapActions } from "pinia";
+import { useTodosStore } from "@/stores/todos";
+import TodoList from "@/components/TodoList.vue";
+
+export default {
+  components: {
+    TodoList,
+  },
+
+  methods: {
+    ...mapActions(useTodosStore, ["archiveTodos"]),
+  },
+
+  mounted() {
+    //  TODO: update on date change if app is left running (set a timer? interval?)
+    this.archiveTodos();
+  },
+};
+</script>
+
+<style></style>
