a9bdd82dd745c245f7af3269e71ed6f4a4a54943
Thomas Kilgour <thomas@kilgour.co>
2022-03-22 15:39:54 -0400

    move svg title attr to elem

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -67,8 +67,8 @@
           viewBox="0 0 24 24"
           stroke="currentColor"
           stroke-width="2"
-          title="Drag to Re-order"
         >
+          <title>Drag to Re-order</title>
           <path
             stroke-linecap="round"
             stroke-linejoin="round"
